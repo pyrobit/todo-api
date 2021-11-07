2b3d19eb421382e6212a6e2ba59af707d9a327ca
Bruno de Lima <pyrobit@gmail.com>
2021-11-06 21:15:16 -0300

    initial features

--- src/index.js
+++ src/index.js
@@ -1,29 +1,87 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username );
+
+  if(!user) {
+    return response.status(400).json({error: "Username not found."});
+  }
+
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+  const userExists = users.some( 
+    (user) => user.username === username
+  );
+
+  if (userExists) {
+    return response.status(400).json({error: "This username is already being used."});
+  }
+
+  const id = uuidv4();
+  const todos = [];
+
+  users.push({
+    id,
+    name,
+    username,
+    todos,
+    
+  });
+
+  return response.status(201).json({
+    id,
+    name,
+    username,
+    todos
+  });
+
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {username} = request.headers;
+  const { user } = request;
+  
+  return response.json(
+    user.todos
+  );
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { title, deadline } = request.body;
+
+  // const id = uuidv4();
+
+  const todo ={
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+
+  };
+
+  user.todos.push(todo);
+
+  return response.status(201).send(todo);
+
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
